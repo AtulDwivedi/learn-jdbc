906f569fa5b91838a4abf6bb8313b7ce83582931
Atul Dwivedi <AtDwivedi@manh.com>
2016-11-10 21:36:05 +0530

    Added example for batch processing

--- src/com/atuldwivedi/learnjdbc/using/batchprocessing/BatchProcessingUsingStatement.java
+++ src/com/atuldwivedi/learnjdbc/using/batchprocessing/BatchProcessingUsingStatement.java
@@ -1,5 +1,28 @@
 package com.atuldwivedi.learnjdbc.using.batchprocessing;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.atuldwivedi.learnjdbc.util.connection.ConnectionFactory;
+
 public class BatchProcessingUsingStatement {
 
+	static Connection con;
+	static Statement stmt;
+
+	public static void main(String[] args) {
+		try {
+			con = ConnectionFactory.getConnecction();
+			stmt = con.createStatement();
+			stmt.addBatch("insert into learnjdbc_coffee values('Cof1', 0, 0, 10.10)");
+			stmt.addBatch("insert into learnjdbc_coffee values('Cof2', 0, 0, 20.10)");
+			int[] i = stmt.executeBatch();
+			for (int j = 0; j < i.length; j++) {
+				System.out.println(i[j]);
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
